052d33b36a4e96fb3428120a9743fe97003d90c4
Mingrui Yu <1249682625@qq.com>
2026-05-06 20:46:00 +0800

    update

--- script/AnyScaleGrasp_DGN.sh
+++ script/AnyScaleGrasp_DGN.sh
@@ -2,8 +2,8 @@
 
 N_WORKER=${1:-12}
 
-# python src/main.py task=official func=proc data=DGN_2k
+python src/main.py task=official func=proc data=DGN_2k
 # python src/main.py task=scene_cfg func=proc data=DGN_2k n_worker=$N_WORKER
 # python src/main.py func=stat data=DGN_2k n_worker=$N_WORKER
 # python src/main.py func=split data=DGN_2k n_worker=$N_WORKER
-python src/main.py func=render data=DGN_2k n_worker=$N_WORKER func.gpu_lst=[0,1,2,3,4,5,6,7]
+# python src/main.py func=render data=DGN_2k n_worker=$N_WORKER func.gpu_lst=[0,1,2,3,4,5,6,7]
